263248f42b9909c91ef1a862b68921a7f9b41c33
Rusi Li <rusili56@gmail.com>
2017-02-17 21:57:49 -0500

    More base code

---
+++ app/src/main/java/nyc/c4q/rusili/grantme/actionbar/CustomActionBar.java
@@ -0,0 +1,18 @@
+package nyc.c4q.rusili.grantme.actionbar;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+
+import nyc.c4q.rusili.grantme.R;
+
+public class CustomActionBar {
+
+    public void modify(Activity activityParam){
+        android.support.v7.app.ActionBar actionBar = ((AppCompatActivity) activityParam).getSupportActionBar();
+        actionBar.setCustomView(R.layout.actionbar_layout);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#00000000")));
+    }
+}

705ff4cdcb198fb4b623477bd9a69e9f57a6b1fb
Rusi Li <rusili56@gmail.com>
2017-02-17 21:53:21 -0500

    More base code

--- app/src/main/java/nyc/c4q/rusili/grantme/fragments/startscreen/FragmentLogin.java
+++ app/src/main/java/nyc/c4q/rusili/grantme/fragments/startscreen/FragmentLogin.java
@@ -1,6 +1,17 @@
 package nyc.c4q.rusili.grantme.fragments.startscreen;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class FragmentLogin extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }

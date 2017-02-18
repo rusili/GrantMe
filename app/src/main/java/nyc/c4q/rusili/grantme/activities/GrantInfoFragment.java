685891ae8cc55c5b3bd166321ddb938559a552c6
joseegonzalez <mathcore@gmail.com>
2017-02-18 15:00:33 -0500

    first step navigation drawer working

---
+++ app/src/main/java/nyc/c4q/rusili/grantme/activities/GrantInfoFragment.java
@@ -0,0 +1,23 @@
+package nyc.c4q.rusili.grantme.activities;
+
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.app.Fragment;
+
+import nyc.c4q.rusili.grantme.R;
+
+/**
+ * Created by mathcore on 2/18/17.
+ */
+
+public class GrantInfoFragment extends Fragment {
+
+
+    public void grantInfo() {
+        String url = "http://mtprawvwsbswtp.nyc.gov/popups/ITG.aspx";
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(getActivity(), Uri.parse(url));
+        builder.setToolbarColor(getResources().getColor(R.color.colorPrimary));
+    }
+}
--- app/src/main/java/nyc/c4q/rusili/grantme/activities/HomePage.java
+++ app/src/main/java/nyc/c4q/rusili/grantme/activities/HomePage.java
@@ -2,21 +2,33 @@ package nyc.c4q.rusili.grantme.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import nyc.c4q.rusili.grantme.R;
 
 public class HomePage extends AppCompatActivity {
     TabLayout mTabLayout;
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
 
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        addDrawerItems();
+
+
+    }
+    private void addDrawerItems() {
+        String[] osArray = { "Profile", "Settings", "Saved Items", "Email", "Random" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
     }
 
     @Override
@@ -32,6 +44,9 @@ public class HomePage extends AppCompatActivity {
         mTabLayout.addTab(mTabLayout.newTab().setText("Tab 2"));
         mTabLayout.addTab(mTabLayout.newTab().setText("Donate"));
         mTabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+
+
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setOffscreenPageLimit(2);

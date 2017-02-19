521d8e6b73cc4f5c8205ce78dad8443a59e3e4d2
nuily <mslinhui@gmail.com>
2017-02-19 17:07:51 -0500

    not best res pics but showing all

--- app/src/main/java/nyc/c4q/rusili/grantme/activities/LocationViewHolder.java
+++ app/src/main/java/nyc/c4q/rusili/grantme/activities/LocationViewHolder.java
@@ -3,7 +3,10 @@ package nyc.c4q.rusili.grantme.activities;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import nyc.c4q.rusili.grantme.R;
 
@@ -14,6 +17,7 @@ public class LocationViewHolder extends RecyclerView.ViewHolder {
 
     private final TextView cardTV;
     private final View cardView;
+    private final ImageView cardImgV;
 
     private CardView mCardView;
 
@@ -22,10 +26,31 @@ public class LocationViewHolder extends RecyclerView.ViewHolder {
         cardView = itemView;
         cardTV = (TextView) itemView.findViewById(R.id.card_tv);
         mCardView = (CardView) itemView.findViewById(R.id.location_card);
+        cardImgV = (ImageView) itemView.findViewById(R.id.card_background);
     }
 
     public void bind(String cardIdentifier ) {
         cardTV.setText(cardIdentifier);
+        switch (cardIdentifier) {
+            case "Brooklyn":
+                Picasso.with(itemView.getContext()).load(R.drawable.brooklyn).into(cardImgV);
+                break;
+            case "Bronx":
+                Picasso.with(itemView.getContext()).load(R.drawable.bronx).into(cardImgV);
+                break;
+            case "Manhattan":
+                Picasso.with(itemView.getContext()).load(R.drawable.manhattan).into(cardImgV);
+                break;
+            case "Queens":
+                Picasso.with(itemView.getContext()).load(R.drawable.queens).into(cardImgV);
+                break;
+            case "Staten Island":
+                Picasso.with(itemView.getContext()).load(R.drawable.statenisland).into(cardImgV);
+                break;
+            case "All":
+                Picasso.with(itemView.getContext()).load(R.drawable.nyc).into(cardImgV);
+                break;
+        }
 
     }
 

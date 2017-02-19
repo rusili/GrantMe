71740df8de21610feab2f46ec41acea6a9d18c9f
nuily <mslinhui@gmail.com>
2017-02-19 13:35:15 -0500

    expand description completed

--- app/src/main/java/nyc/c4q/rusili/grantme/recyclerview/CourseViewholder.java
+++ app/src/main/java/nyc/c4q/rusili/grantme/recyclerview/CourseViewholder.java
@@ -2,6 +2,7 @@ package nyc.c4q.rusili.grantme.recyclerview;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import nyc.c4q.rusili.grantme.R;
@@ -9,6 +10,8 @@ import nyc.c4q.rusili.grantme.network.pojo.JSONCourses;
 
 public class CourseViewholder extends RecyclerView.ViewHolder{
 
+    private final ImageButton expandBtn;
+    private TextView mDescription;
     private TextView mCourseName;
     private TextView mWebSite;
     private TextView mBorough;
@@ -17,8 +20,10 @@ public class CourseViewholder extends RecyclerView.ViewHolder{
     public CourseViewholder(View itemView) {
         super(itemView);
         mCourseName = (TextView) itemView.findViewById(R.id.course_name);
+        mDescription = (TextView) itemView.findViewById(R.id.description);
         mWebSite = (TextView) itemView.findViewById(R.id.web_site);
         mBorough = (TextView) itemView.findViewById(R.id.borough);
+        expandBtn = (ImageButton) itemView.findViewById(R.id.expand_btn);
     }
 
     public void bind (JSONCourses course) {
@@ -26,6 +31,20 @@ public class CourseViewholder extends RecyclerView.ViewHolder{
         mCourseName.setText(course.getCourseName());
         mWebSite.setText(course.getWebsite());
         mBorough.setText(course.getBorough());
+        mDescription.setText(course.getCoursedescription());
+        expandBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mDescription.getVisibility() == View.GONE) {
+                    mDescription.setVisibility(View.VISIBLE);
+                    expandBtn.setImageResource(R.drawable.ic_keyboard_arrow_up_black_24dp);
+                } else {
+                    mDescription.setVisibility(View.GONE);
+                    expandBtn.setImageResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
+                }
+            }
+        });
+
 
 
     }
